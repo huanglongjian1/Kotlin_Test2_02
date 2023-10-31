d42047a127a565d28c2c3ebf2c5419ce19cc228e
huanglongjian1 <527921342@qq.com>
2023-10-31 12:45:51 +0800

    two

---
+++ app/src/main/java/com/example/kotlin_test2/test1/fragment04/Test1Fragment04.kt
@@ -0,0 +1,34 @@
+package com.example.kotlin_test2.test1.fragment04
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlin_test2.R
+import com.example.kotlin_test2.util.Loge
+
+class Test1Fragment04 : Fragment() {
+
+    companion object {
+        fun newInstance() = Test1Fragment04()
+    }
+
+    private lateinit var viewModel: Test1Fragment04ViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Loge.e("第一次")
+        return inflater.inflate(R.layout.fragment_test1_fragment04, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(Test1Fragment04ViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/kotlin_test2/test1/fragment04/Test1Fragment04ViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin_test2.test1.fragment04
+
+import androidx.lifecycle.ViewModel
+
+class Test1Fragment04ViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
